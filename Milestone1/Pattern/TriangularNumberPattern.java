7663bc4a8cc1667506b191e92578c565e5186756
Parambir Kumar <114753610+Mrparam07@users.noreply.github.com>
2022-11-09 19:07:02 +0530

    Create TriangularNumberPattern.java

---
+++ Milestone1/Pattern/TriangularNumberPattern.java
@@ -0,0 +1,57 @@
+/*
+Code : Triangle Number Pattern
+Send Feedback
+Print the following pattern for the given N number of rows.
+Pattern for N = 4
+1
+22
+333
+4444
+Input format :
+Integer N (Total no. of rows)
+Output format :
+Pattern in N lines
+Constraints
+0 <= N <= 50
+Sample Input 1:
+5
+Sample Output 1:
+1
+22
+333
+4444
+55555
+Sample Input 2:
+6
+Sample Output 2:
+1
+22
+333
+4444
+55555
+666666
+*/
+
+
+import java.util.Scanner;
+public class Solution {
+
+
+	public static void main(String[] args) {
+		
+	Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+	int i=1;
+	while(i<=n){
+		int j=1;
+		while(j<=i){
+			System.out.print(i);
+			j++;
+		}
+		i++;
+		System.out.println();
+	}
+		
+	}
+
+}
