4ca2c1781f58f4ef817f437ad8587c3b9b0dcef4
Parambir Kumar <114753610+Mrparam07@users.noreply.github.com>
2022-11-12 16:19:26 +0530

    12-11-22

---
+++ Milestone1/Pattern2/TriangleOfNumbers.java
@@ -0,0 +1,34 @@
+/*
+Code : Triangle of Numbers
+Send Feedback
+Print the following pattern for the given number of rows.
+Pattern for N = 4
+
+
+
+The dots represent spaces.
+
+
+
+Input format :
+Integer N (Total no. of rows)
+Output format :
+Pattern in N lines
+Constraints :
+0 <= N <= 50
+Sample Input 1:
+5
+Sample Output 1:
+     1 
+    232
+   34543
+  4567654
+ 567898765
+Sample Input 2:
+4
+Sample Output 2:
+     1
+    232
+   34543
+  4567654
+*/
