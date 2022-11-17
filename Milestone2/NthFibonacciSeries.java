d78011994662d0f789142af5ccbf7b2ea1f8b615
Parambir Kumar <114753610+Mrparam07@users.noreply.github.com>
2022-11-17 19:19:41 +0530

    16-11-22

--- Milestone2/NthFibonacciSeries.java
+++ Milestone2/NthFibonacciSeries.java
@@ -17,3 +17,22 @@ public class Solution {
         
 		
 	}
+//method 2 using array
+
+import java.util.Scanner;
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+        int arr[]=new int[n];
+        arr[0]=1;
+        arr[1]=1;
+        for(int i=2;i<n;i++){
+
+            arr[i]=arr[i-1]+arr[i-2];
+        }
+        System.out.println(arr[n-1]);
+    }
+}
+
