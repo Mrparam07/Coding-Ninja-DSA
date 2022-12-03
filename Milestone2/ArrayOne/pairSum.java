4cc90c02eb61e0abb3388406e5ed577616e24aa6
Parambir Kumar <114753610+Mrparam07@users.noreply.github.com>
2022-12-03 16:34:34 +0530

    03-12-22

---
+++ Milestone2/ArrayOne/pairSum.java
@@ -0,0 +1,17 @@
+public class Solution {  
+
+    public static int pairSum(int arr[], int x) {
+    	//Your code goes here
+        int count=0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[i] + arr[j] == x) {
+                    count++;
+                 
+                }
+            }
+        }
+        return count;
+    }
+}
