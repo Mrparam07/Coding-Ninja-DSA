d52030084acd3787f8dde1052453d386af05cd9b
Parambir Kumar <114753610+Mrparam07@users.noreply.github.com>
2023-01-26 14:46:30 +0530

    26-01-23

--- Milestone3/Recursion/sumOfArrays.java
+++ Milestone3/Recursion/sumOfArrays.java
@@ -3,11 +3,14 @@ public class Solution {
 	int n;
 	int result;
 	public static int sum(int input[]) {
-		int output=0;
-		for(int i=0;i<input.length;i++)
+		if(input.length==0)
+		return 0;
+		int arr[]=new int[input.length-1];
+		for(int i=1;i<input.length;i++)
 		{
-			output+=input[i];
+			arr[i-1]=input[i];
 		}
+		int output=input[0]+sum(arr);
 		return output;
 	}
 }
