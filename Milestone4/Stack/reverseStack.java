b4d983e9fa5dd19fb357712a68f98484ea297505
Parambir Kumar <114753610+Mrparam07@users.noreply.github.com>
2023-03-17 17:48:43 +0530

    17-03-23

---
+++ Milestone4/Stack/reverseStack.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+public class Solution {
+
+	public static void reverseStack(Stack<Integer> input, Stack<Integer> extra) {
+		//Your code goes here
+		if(input.size()==0 || input.size()==1){
+			return;
+		}
+		int top=input.pop();
+		reverseStack(input, extra);
+		while(!input.isEmpty())
+		{
+			extra.push(input.pop());
+		}
+		input.push(top);
+		while(!extra.isEmpty())
+		{
+			input.push(extra.pop());
+		}
+	}
+}
