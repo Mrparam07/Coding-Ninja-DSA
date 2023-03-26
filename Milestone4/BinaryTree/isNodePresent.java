c2d924162b2eb5ffe2dcf4fe33cbb1f3ac3e7d2d
Parambir Kumar <114753610+Mrparam07@users.noreply.github.com>
2023-03-26 17:29:42 +0530

    26-03-23

---
+++ Milestone4/BinaryTree/isNodePresent.java
@@ -0,0 +1,32 @@
+/*
+	
+	Following is the structure used to represent the Binary Tree Node
+
+	class BinaryTreeNode<T> {
+		T data;
+		BinaryTreeNode<T> left;
+		BinaryTreeNode<T> right;
+
+		public BinaryTreeNode(T data) {
+			this.data = data;
+			this.left = null;
+			this.right = null;
+		}
+	}
+
+*/
+
+public class Solution {
+
+	public static boolean isNodePresent(BinaryTreeNode<Integer> root, int x) {
+	    //Your code goes here
+		if(root == null)
+		return false;
+
+		if(root.data == x)
+		return true;
+		else
+		return (isNodePresent(root.left, x) || isNodePresent(root.left, x));
+	}
+
+}
