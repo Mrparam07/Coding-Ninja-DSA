5a0173fdc7904587a69fb96e63552e741b1adaeb
Parambir Kumar <114753610+Mrparam07@users.noreply.github.com>
2023-04-02 02:10:52 +0530

    01-04-23

---
+++ Milestone4/BST/searchNode.java
@@ -0,0 +1,25 @@
+public class Solution {
+
+	/*
+	 * Binary Tree Node class
+	 * 
+	 * class BinaryTreeNode<T> { T data; BinaryTreeNode<T> left; BinaryTreeNode<T> right;
+	 * 
+	 * public BinaryTreeNode(T data) { this.data = data; } }
+	 */
+
+
+	public static boolean searchInBST(BinaryTreeNode<Integer> root, int k) {
+		if(root == null)
+		return false;
+
+		if(root.data == k)
+		return true;
+		if(k>root.data)
+		return searchInBST(root.right, k);
+		else
+		return searchInBST(root.left, k);
+
+		
+	}
+}
