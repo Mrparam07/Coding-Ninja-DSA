8ddcb45f816ea3d71414ba9ce804da69f5b12f53
Parambir Kumar <114753610+Mrparam07@users.noreply.github.com>
2023-04-04 22:58:16 +0530

    04-04-23

--- Milestone4/BST/CheckifBinaryTreeisBST.java
+++ Milestone4/BST/CheckifBinaryTreeisBST.java
@@ -16,23 +16,54 @@
 
  */
 
+// public class Solution {
+
+// 	public static boolean isBST(BinaryTreeNode<Integer> root) {
+
+// 		if(root==null)
+//              return true;
+//           if(root.left!=null && root.right!=null){
+// 			if(root.data<=root.left.data || root.data>=root.right.data)
+//               return false;
+// 		  }
+//             // if(root.data>root.left.data && root.data<=root.right.data)
+//             //   return true;
+                
+//      //   }
+//             boolean ans1=isBST(root.left);
+//             boolean ans2=isBST(root.right);
+//             return (ans1&& ans2);
+// 	}
+
+// }
 public class Solution {
 
 	public static boolean isBST(BinaryTreeNode<Integer> root) {
+         if(root==null)
+                return true;
+            int max=maximum(root.left);
+            int min=minimum(root.right);
+            if(root.data<=max || root.data>min)
+                return false;
 
-		if(root==null)
-             return true;
-          if(root.left!=null && root.right!=null){
-			if(root.data<=root.left.data || root.data>=root.right.data)
-              return false;
-		  }
-            // if(root.data>root.left.data && root.data<=root.right.data)
-            //   return true;
-                
-     //   }
             boolean ans1=isBST(root.left);
             boolean ans2=isBST(root.right);
-            return (ans1&& ans2);
-	}
+            return(ans1&& ans2);
 
-}
+        }
+        public static int maximum(BinaryTreeNode<Integer> root)
+        {
+            if(root==null)
+                return Integer.MIN_VALUE;
+            return Math.max(root.data,Math.max(maximum(root.left),maximum(root.right)));
+
+
+            }
+        public static int minimum(BinaryTreeNode<Integer> root)
+        {
+            if(root==null)
+                return Integer.MAX_VALUE;
+            return Math.min(root.data,Math.min(minimum(root.left),minimum(root.right)));
+        }
+
+    }
