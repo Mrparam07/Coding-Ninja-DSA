719c517b960ff2e0292b75597d67f8347cc96c93
Parambir Kumar <114753610+Mrparam07@users.noreply.github.com>
2023-05-30 21:26:27 +0530

    30-05-23

---
+++ Milestone5/Dynamic-Programming/coinTower.java
@@ -0,0 +1,25 @@
+public class Solution {
+
+	public static String findWinner(int n, int x, int y) {
+		//Your code goes here
+		int dp[] = new int[n+1];
+		dp[0] = 0;
+		dp[1] = 1;
+		for(int i=2;i<=n;i++)
+		{
+			if(dp[i-1] == 0)
+			dp[i] = 1;
+			else if((i-x)>=0 && dp[i-x] == 0)
+			dp[i] = 1;
+			else if((i-y)>=0 && dp[i-y] == 0)
+			dp[i] = 1;
+			else
+			dp[i] = 0;
+		}
+		if(dp[n] == 1)
+		return "Beerus";
+		else
+		return "Whis";
+	}
+
+}
