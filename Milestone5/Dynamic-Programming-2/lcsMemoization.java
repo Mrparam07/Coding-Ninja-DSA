6736587ba6edc132b1d1199a2449bde5ef1a1c7a
Parambir Kumar <114753610+Mrparam07@users.noreply.github.com>
2023-05-31 20:41:57 +0530

    31-05-23

--- Milestone5/Dynamic-Programming-2/lcsMemoization.java
+++ Milestone5/Dynamic-Programming-2/lcsMemoization.java
@@ -1,64 +1,52 @@
 
+
 public class Solution {
 
 	public static int lcs(String s, String t) {
 		//Your code goes here
-		int dp[][] = new int[s.length()+1][t.length()+1];
-		for(int i=0;i<dp.length;i++)
-		{
-			for(int j=0;j<dp[0].length;j++)
-			{
-				dp[i][j] = -1;
-			}
-		}
-		return helperA(s,t,0,0,dp);
+        int[][] dp = new int[s.length()+1][t.length()+1];
+        for (int i=0;i<dp.length;i++)
+        {
+            for (int j=0;j<dp[0].length;j++)
+            {
+                dp[i][j]=-1;
+            }
+        }
+        return lcsHelper(s,0,t,0,dp);
     }
-	public static int helperA(String s, String t, int i, int j,int dp[][])
-	{
-		if(i==s.length() || j == t.length())
-		return 0;
-		int myAns;
-		
-		if(s.charAt(i) == t.charAt(j))
-		{
-			int smallAns;
-			if(dp[i+1][j+1] == -1){
-			smallAns = helper(s,t,i+1,j+1);
-			dp[i+1][j+1] = smallAns;
-			}
-			else
-			smallAns = dp[i+1][j+1];
-
-			myAns = 1+smallAns;
-		}
-		else{
-		int ans1, ans2;
-		if(dp[i+1][j] == -1){
-			ans1 = helper(s,t,i+1,j);
-			dp[i+1][j] = ans1;
-		}
-		else
-		 ans1= dp[i+1][j];
-
-		 if(dp[i][j+1] == -1){
-			ans2 = helper(s,t,i,j+1);
-			dp[i][j+1] = ans2;
-		}
-		else
-		 ans2= dp[i][j+1];
-		myAns =  Math.max(ans1,ans2);
-		}
-		return myAns;
-	}
-	public static int helper(String s, String t, int i, int j)
-	{
-		if(i==s.length() || j == t.length())
-		return 0;
-
-		if(s.charAt(i) == t.charAt(j))
-		return 1 + helper(s,t,i+1,j+1);
-		else
-		return Math.max(helper(s,t,i+1,j), helper(s,t,i,j+1));
-	}
+    
+    private static int lcsHelper(String s, int i, String t, int j, int[][] dp)
+    {
+        if (i==s.length() || j== t.length())
+        {
+            return 0;
+        }
+        
+        if (s.charAt(i)==t.charAt(j))
+        {
+            if (dp[i+1][j+1]==-1)
+            {
+                dp[i+1][j+1]=lcsHelper(s,i+1,t,j+1,dp);
+            }
+            dp[i][j]=1+dp[i+1][j+1];
+        }
+        else
+        {
+            if(dp[i+1][j]==-1)
+            {
+                dp[i+1][j]=lcsHelper(s,i+1,t,j,dp);
+            }
+            int ans1=dp[i+1][j];
+            
+            if(dp[i][j+1]==-1)
+            {
+                dp[i][j+1]=lcsHelper(s,i,t,j+1,dp);
+            }
+            int ans2=dp[i][j+1];
+            
+            dp[i][j]=Math.max(ans1,ans2);
+        }
+        return dp[i][j];
+    }
 
 }
